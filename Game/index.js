1371e29a40539f58db4646ef6ab75e89cc01acb4
sennrdesign <sennadeturner@gmail.com>
2017-03-02 13:32:42 +0100

    icon working now

--- Game/index.js
+++ Game/index.js
@@ -19,7 +19,7 @@ function Game(playground_div, width, height){
 	GDiv.style.height = this.height+"px";
 	GDiv.style.overflow = 'hidden';
 	GDiv.style.border = '2px solid #aaa';
-	GDiv.style.cursor = 'url("game.ico")';
+	GDiv.style.cursor = 'url("game.ico"),auto';
 	GDiv.className = 'noselect';
 	this.Start = function(gamecode){
 		var button = document.createElement("button");
